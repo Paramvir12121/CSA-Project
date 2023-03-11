3a2c635084cf12a04e6113ea450aa019c78a1e60
Paramvir12121 <paramvir12121@gmail.com>
2023-03-10 23:41:58 -0500

    base app

---
+++ react-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+//import "./App.css";
+import { Login, Signup, Navbar, Footer } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+      <Login />
+      <Signup />
+    </div>
+  );
+};
+
+export default App;
